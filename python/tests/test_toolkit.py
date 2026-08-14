54bdf96a990b54d9b7b9f83fed3beefd75aae967
Damian Sobanski <d.sobanski@hotmail.com>
2026-08-14 14:20:05 +0200

    test added

--- python/tests/test_toolkit.py
+++ python/tests/test_toolkit.py
@@ -3,4 +3,16 @@ import numpy as np
 from tools.toolkit import update_estimate
 
 def test_update_estimate_first_reward():
-    return True
+    new_estimate = update_estimate(reward=1, n=1, q_old=0)
+
+    assert new_estimate == 1
+
+def test_update_estimate_raises_on_zero_n():
+    with pytest.raises(ValueError):
+        update_estimate(reward=1, n = 0, q_old=0)
+
+def test_averaging_reward():
+    new_estimate  = update_estimate(reward=1, n=1, q_old=0)
+    new_estimate2 = update_estimate(reward=1, n=2, q_old=new_estimate)
+
+    assert new_estimate2 == 1
